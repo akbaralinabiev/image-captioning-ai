f7ebb20a0a6f9032e63b1e1f5d822a83a6ef0fb8
Akbarali Nabiev <akbaralinabiev14@gmail.com>
2024-04-28 23:55:29 +0200

    new loader added

---
+++ js/loading.js
@@ -0,0 +1,29 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    setTimeout(function () {
+      document.getElementById("loading").className = "slideDown";
+    }, 3000);
+  },
+  false
+);
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    setTimeout(function () {
+      document.getElementById("loading-center").className = "zoomOut";
+    }, 2800);
+  },
+  false
+);
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    setTimeout(function () {
+      document.getElementById("block-1").className = "slideInUp";
+    }, 3000);
+  },
+  false
+);
